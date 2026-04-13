5358a0f27fd94691427c48349760ca3ce8890e5b
Varto Yazılım <teknik@vartoyazilim.com>
2026-04-13 12:45:43 +0300

    v1.1
    Vercel Analytics add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Manrope, Plus_Jakarta_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
@@ -45,6 +46,7 @@ export default function RootLayout({
             <SiteFooter />
           </div>
         </StoreProvider>
+        <Analytics />
       </body>
     </html>
   );
